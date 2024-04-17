0e51100a0edcfe8a31d0b34c1150bf8ce00c7ae6
Suham <arhaancareer@gmail.com>
2024-04-17 14:05:28 +0530

    proper image

--- src/lib/data.js
+++ src/lib/data.js
@@ -12,14 +12,14 @@ const posts = [
   {
     title: "Mr",
     desc: "Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.\n\nNam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.\n\nCurabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.",
-    img: "https://unsplash.com/photos/a-bird-flying-in-the-sky-with-a-rainbow-in-the-background-0-I2BoCRr1o",
+    img: "https://plus.unsplash.com/premium_photo-1706625700445-992ed7a4a5e7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     userId: 1,
     slug: "post-1",
   },
   {
     title: "Mrs",
     desc: "Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.\n\nSed ante. Vivamus tortor. Duis mattis egestas metus.",
-    img: "https://unsplash.com/photos/a-bird-flying-in-the-sky-with-a-rainbow-in-the-background-0-I2BoCRr1o",
+    img: "https://plus.unsplash.com/premium_photo-1706625700445-992ed7a4a5e7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 
     userId: 2,
     slug: "post-2",
@@ -27,7 +27,7 @@ const posts = [
   {
     title: "Mrs",
     desc: "Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.\n\nIn quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.",
-    img: "https://unsplash.com/photos/a-bird-flying-in-the-sky-with-a-rainbow-in-the-background-0-I2BoCRr1o",
+    img: "https://plus.unsplash.com/premium_photo-1706625700445-992ed7a4a5e7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 
     userId: 3,
     slug: "product-2",
@@ -35,7 +35,7 @@ const posts = [
   {
     title: "Ms",
     desc: "In congue. Etiam justo. Etiam pretium iaculis justo.",
-    img: "https://unsplash.com/photos/a-bird-flying-in-the-sky-with-a-rainbow-in-the-background-0-I2BoCRr1o",
+    img: "https://plus.unsplash.com/premium_photo-1706625700445-992ed7a4a5e7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 
     userId: 4,
     slug: "page-2",
@@ -43,7 +43,7 @@ const posts = [
   {
     title: "Dr",
     desc: "Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem.",
-    img: "https://unsplash.com/photos/a-bird-flying-in-the-sky-with-a-rainbow-in-the-background-0-I2BoCRr1o",
+    img: "https://plus.unsplash.com/premium_photo-1706625700445-992ed7a4a5e7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 
     userId: 5,
     slug: "product-2",
@@ -51,7 +51,7 @@ const posts = [
   {
     title: "Honorable",
     desc: "In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.\n\nMaecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.\n\nMaecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.",
-    img: "https://unsplash.com/photos/a-bird-flying-in-the-sky-with-a-rainbow-in-the-background-0-I2BoCRr1o",
+    img: "https://plus.unsplash.com/premium_photo-1706625700445-992ed7a4a5e7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 
     userId: 6,
     slug: "product-1",
@@ -59,7 +59,7 @@ const posts = [
   {
     title: "Honorable",
     desc: "Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.",
-    img: "https://unsplash.com/photos/a-bird-flying-in-the-sky-with-a-rainbow-in-the-background-0-I2BoCRr1o",
+    img: "https://plus.unsplash.com/premium_photo-1706625700445-992ed7a4a5e7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 
     userId: 7,
     slug: "page-1",
@@ -67,7 +67,7 @@ const posts = [
   {
     title: "Dr",
     desc: "Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.",
-    img: "https://unsplash.com/photos/a-bird-flying-in-the-sky-with-a-rainbow-in-the-background-0-I2BoCRr1o",
+    img: "https://plus.unsplash.com/premium_photo-1706625700445-992ed7a4a5e7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 
     userId: 8,
     slug: "post-2",
@@ -75,7 +75,7 @@ const posts = [
   {
     title: "Mrs",
     desc: "Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.",
-    img: "https://unsplash.com/photos/a-bird-flying-in-the-sky-with-a-rainbow-in-the-background-0-I2BoCRr1o",
+    img: "https://plus.unsplash.com/premium_photo-1706625700445-992ed7a4a5e7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 
     userId: 9,
     slug: "page-2",
@@ -83,7 +83,7 @@ const posts = [
   {
     title: "Rev",
     desc: "Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.",
-    img: "https://unsplash.com/photos/a-bird-flying-in-the-sky-with-a-rainbow-in-the-background-0-I2BoCRr1o",
+    img: "https://plus.unsplash.com/premium_photo-1706625700445-992ed7a4a5e7?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 
     userId: 10,
     slug: "post-1",
